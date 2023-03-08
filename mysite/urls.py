7ba05d398d2ffe6cd8249742f8696ff265053381
lillyengeler <lre250@nyu.edu>
2023-03-08 00:20:58 -0500

    noqa

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from django.conf.urls import url
+from django.conf.urls import url # noqa F401
 
 urlpatterns = [
     path('admin/', admin.site.urls),
