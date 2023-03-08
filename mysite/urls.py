b9bf99bafe0c051d25c82a3249cb11b41ff16208
lillyengeler <lre250@nyu.edu>
2023-03-08 00:54:47 -0500

    fixing django version

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
+from django.urls import include, re_path
 from django.conf.urls import url 
 
 urlpatterns = [
